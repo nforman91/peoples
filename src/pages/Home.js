7bc72289663e62c2ebc59b7bddd57796215fb6ed
Nathan <nathan.forman@gmail.com>
2023-12-01 16:25:35 -0800

    Getting further with planet page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,10 +9,16 @@ const Home = () => {
     return (
         <div>
             <StyledWaterWorks>Water Works Amphitheater</StyledWaterWorks>
-            <StyledHush>HUSH</StyledHush>
-            <StyledFestivals>Festivals</StyledFestivals>
-            {/* <Planets>HUSH</Planets> */}
+            <StyledTwo>
+                <StyledServices>Services</StyledServices>
+                <StyledHush>HUSH</StyledHush>
+            </StyledTwo>
+            <StyledTBD>TBD</StyledTBD>
             <StyledPeoples><img src={peoples_bw_on_white_logo} alt="People's Productions logo"/></StyledPeoples>
+            <StyledFestivals>Festivals</StyledFestivals>
+            
+            {/* <Planets>HUSH</Planets> */}
+            
             {/* <StyledHush><img src={} alt="People's Productions logo"/></StyledHush> */}
         </div>
     );
@@ -33,7 +39,41 @@ const StyledWaterWorks = styled.div`
     margin: 2vh 0 0 80vh;
 `;
 
+const StyledTwo = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: max-content;
+`;
+
+const StyledServices = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 10vh;
+    height: 10vh;
+    line-height: 5vh;
+    font-size: 20px;
+    border-radius: 50%;
+    text-align: center;
+    border: 1px solid black;
+    margin: 5vh 0 0 5vh;
+`;
+
 const StyledHush = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 10vh;
+    height: 10vh;
+    line-height: 5vh;
+    font-size: 20px;
+    border-radius: 50%;
+    text-align: center;
+    border: 1px solid black;
+    margin: 5vh 0 0 130vh;
+`;
+
+const StyledTBD = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -45,7 +85,7 @@ const StyledHush = styled.div`
     border-radius: 50%;
     text-align: center;
     border: 1px solid black;
-    margin: 5vh 0 0 130vh;
+    margin: 5vh 0 0 5vh;
 `;
 
 const StyledFestivals = styled.div`
