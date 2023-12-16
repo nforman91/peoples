c02c4cb0d19bf6fe9787e9ba9c4422e8e3ee4b19
Nathan <nathan.forman@gmail.com>
2023-12-15 17:13:59 -0800

    Made Festivals modal scrollable

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,7 @@ import peoples_bw_on_white_logo from "../img/peoples_bw_on_white_logo.jpg";
 // import planets from "../planets/Planets";
 // import Info from "../components/Info";
 import blue_planet from "../img/blue_planet.jpg";
-import YouTube from "react-youtube";
 import ReactPlayer from "react-player";
-import ButtonMailTo from "../components/ButtonMailTo";
 
 const Home = () => {
     const [waterModal, setWaterModal] = useState(false);
@@ -114,15 +112,11 @@ const Home = () => {
                             <StyledHushModalContent>
                                 <h4>HUSH</h4>
                                 <p>HUSH – SILENT DISCO’S are the entertainment / party events sweeping the globe. This new sensation is a dance party with 3 DJ’s at the same time who are playing to a packed crowd in what appears to be total silence.  Participants all wear high-quality noise cancelling headphones and can choose between which DJ they like the best at any given moment.  It’s a spectacle to witness and an even bigger experience to participate in.  Think a packed room of people all grooving to their own beat or singing their favorite song at the top of their lungs with absolutely NO SOUND being played over amplified speakers.  This revolution started in Europe when creative rave organizers needed a solution to their late night parties being constantly shut down due to noise problems.  So they came up with this amazing idea of being able to throw dance parties with no amplified sound and it was an immediate smash hit.  It came to the United States via the festival circuit and it is now a staple part of many many music festivals around the county. We are very excited that we are now able to bring this technology to you at any location.  We do everything from large 15,000 person festivals to back yard / basement or office parties and everything in between.  We have never experienced anything so satisfying that is always a 100% home run with guests and neither will you.<br/><br/>Contact us at 
-                                
-                                {/* <ButtonMailTo label=" HUSHDSM " mailto="HUSHDSM@GMAIL.COM"/>  */}
                                 <br/>
                                 <a href="mailto:HUSHDSM@GMAIL.COM">hushdsm@gmail.com</a>
                                 <br/>
                                 for more details or a quote for your next unforgettable party.<br/><br/>Enough words …  Silent Discos are best described with video evidence</p>
                                 <br/>
-                                {/* <iframe src="https://www.youtube.com/watch?v=elAWsTOl0kM" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen title="video"/> */}
-                                {/* <YouTube videoId="elAWsTOl0kM"/> */}
                                 <ReactPlayer url="https://www.youtube.com/watch?v=elAWsTOl0kM"/>
                                 <StyledHushCloseButton onClick={toggleHushModal}>CLOSE</StyledHushCloseButton>
                             </StyledHushModalContent>
@@ -152,40 +146,41 @@ const Home = () => {
                             <StyledFestivalsOverlay onClick={toggleFestivalsModal}/>
                             <StyledFestivalsModalContent>
                                 <h4>FESTIVALS</h4>
-                                <p>FESTIVALS – Festival Season is our FAVORITE Season !!!  And People’s Productions has their hand in SO Many great festivals here in the Midwest.  We are the festival management Guru’s of Central Iowa.  We have a managing role in 10 different festivals over the course of the year.  We are here to promote the ones that we currently work with and fill you in our services for the ones that we don’t yet.  We are the experts in the field and bring a full staff for whatever is needed to facilitate the success of your event.  Whether you just need help with a couple of aspects of your event or need a managing director our crack staff is on top of everything in the Festie world and can bring you everything from talent procurement and production management to cost savings due to our large volume of events that we produce.  
+                                <p>FESTIVALS – Festival Season is our FAVORITE Season !!!  And People’s Productions has their hand in SO Many great festivals here in the Midwest.  We are the festival management Guru’s of Central Iowa.  We have a managing role in 10 different festivals over the course of the year.  We are here to promote the ones that we currently work with and fill you in our services for the ones that we don’t yet.  We are the experts in the field and bring a full staff for whatever is needed to facilitate the success of your event.  Whether you just need help with a couple of aspects of your event or need a managing director our crack staff is on top of everything in the Festie world and can bring you everything from talent procurement and production management to cost savings due to our large volume of events that we produce.</p>  
 <br/><br/>
-Some of the services that we provide for various festivals : 
-Talent Buying
-Staging Operations
-Production Management (Sound & Lights)
-Vendor Coordination 
-Infrastructure Procurement and Supervision 
-Security Consultation
-Entertainment Services
-Volunteer Program Development 
-Ticketing Oversight and Crowd Control 
-Trash and Clean-Up Services
-Marketing and Media Management 
-Sponsorship Procurement
-Stage Management 
-Parking Services
-Venue Liaison 
-
+<p>Some of the services that we provide for various festivals : </p>
+<ul>
+<li>Talent Buying</li>
+<li>Staging Operations</li>
+<li>Production Management (Sound & Lights)</li>
+<li>Vendor Coordination </li>
+<li>Infrastructure Procurement and Supervision </li>
+<li>Security Consultation</li>
+<li>Entertainment Services</li>
+<li>Volunteer Program Development </li>
+<li>Ticketing Oversight and Crowd Control </li>
+<li>Trash and Clean-Up Services</li>
+<li>Marketing and Media Management </li>
+<li>Sponsorship Procurement</li>
+<li>Stage Management </li>
+<li>Parking Services</li>
+<li>Venue Liaison </li>
+</ul>
 <br/><br/>
-Some of the Festivals that we work with : 
-Blue Ribbon Bacon Festival 
-Hinterland Music Festival 
-515 Alive Music Festival 
-80-35 Music Festival 
-Greenbelt Music Festival 
-Iowa Craft Beer Festival
-Oktoberfest DSM 
-Field Daze Music Series
-Ragbrai 
-Truckin’ Food Truck Festival 
-Des Moines Arts Festival
-</p>
-                                {/* <ReactPlayer url=""/> */}
+<p>Some of the Festivals that we work with : </p>
+<ul>
+<li>Blue Ribbon Bacon Festival </li>
+<li>Hinterland Music Festival </li>
+<li>515 Alive Music Festival </li>
+<li>80-35 Music Festival </li>
+<li>Greenbelt Music Festival </li>
+<li>Iowa Craft Beer Festival</li>
+<li>Oktoberfest DSM </li>
+<li>Field Daze Music Series</li>
+<li>Ragbrai </li>
+<li>Truckin’ Food Truck Festival </li>
+<li>Des Moines Arts Festival</li>
+</ul>
                                 <StyledFestivalsCloseButton onClick={toggleFestivalsModal}>CLOSE</StyledFestivalsCloseButton>
                             </StyledFestivalsModalContent>
                         </StyledFestivalsModal>
@@ -202,7 +197,6 @@ Des Moines Arts Festival
                                 <h4>HUMAN FOOSBALL</h4>
                                 <p>HUMAN FOOSBALL – Yep !  Its just like it sounds …  put a bunch of humans in a giant foosball table and let them kick their way to a championship !  If was only that simple …  but now it IS !  We have built the perfect mousetrap for your backyard party or company event.  Our giant foosball table is an exact replica of a standard foosball table but human sized.  It’s a 6 on 6 battle royale but there’s a no spinning your players rule because the players are all your family / friends / co-workers.  The 2 teams strap themselves into their respective poles and slide back and forth as a team “kicking” their way back and forth to score goals. It is a great activity for all ages …   We have been invited to everything from backyard birthday parties to middle school church functions and corporate team building retreats to St Patrick’s Day festivals and of course the World’s largest Bacon Festival.   We operate our Human Foosball Arena in a couple of different ways.  We can come set it up in your back yard / driveway / indoor gym or really anywhere that you have a 90’ x 40’ area for our layout.  It works on both grass or concrete or any inside surface.  We can either just set it up and let you enjoy at your leisure or we will come and manage it as a tournament complete with referee’s and a bracket system if you want to promote a competition.  Both options are crazy fun and enjoyable and something that your guests will be talking about for years to come.  
 <br/><br/>NEW IN 2024 !!! – We will be hosting a traveling bar league this fall !!! Contact us if you want to be a part of the fun with your team of 6 or if you want us to include your establishment as part of our traveling competition<br/><br/>
-                                {/* <ButtonMailTo label="HUMAN FOOSBALL EMAIL" mailto="HUMANFOOSBALL@GMAIL.COM"/> */}
                                 <a href="mailto:HUMANFOOSBALL@GMAIL.COM">humanfoosball@gmail.com</a>
                                 </p>
                                 <ReactPlayer url="https://www.youtube.com/watch?v=2hhPGexC_98"/>
@@ -622,10 +616,11 @@ const StyledFestivalsModalContent = styled.div`
     /* top: 40%; */
     /* left: 50%; */
     line-height: 1.4;
-    /* padding: 14px 28px; */
+    padding: 5vh;
     border-radius: 3px;
     /* max-width: 600px; */
     min-width: 300px;
+    max-height: calc(100vh - 210px);
     overflow-y: auto;
 `;
 
